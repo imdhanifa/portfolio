defb58cb4ccec3ae07922dffa4a347aa2e57f449
imdhanifa <imdhanifa@gmail.com>
2025-09-25 02:17:21 +0530

    all changed t

--- src/main.tsx
+++ src/main.tsx
@@ -5,16 +5,16 @@ import { store } from "./store/store";
 import App from "./App";
 import "./index.css";
 import { ThemeProvider } from "./context/ThemeContext";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 // ✅ Mount React app
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
       <ThemeProvider>
-        <BrowserRouter>
+        <HashRouter >
           <App />
-        </BrowserRouter>
+        </HashRouter>
       </ThemeProvider>
     </Provider>
   </React.StrictMode>
